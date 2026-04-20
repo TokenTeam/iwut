6ffc8d847f562c92a318881bb1b153939be4c8fd
zhxycn <admin@owo.cab>
2026-04-21 00:58:36 +0800

    :wrench: chore: 调整构建环境配置

--- plugins/with-gradle-props.js
+++ plugins/with-gradle-props.js
@@ -10,8 +10,8 @@ const withGradleProps = createBuildGradlePropsConfigPlugin(
       propName: "org.gradle.jvmargs",
       propValueGetter: () =>
         [
-          "-Xmx4096m",
-          "-XX:MaxMetaspaceSize=1024m",
+          "-Xmx8g",
+          "-XX:MaxMetaspaceSize=2g",
           "-XX:+HeapDumpOnOutOfMemoryError",
         ].join(" "),
     },
